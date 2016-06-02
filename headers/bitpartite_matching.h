f2280650299766bc11b446328b385514c117db70
youngsoo lee <prevdev@gmail.com>
2016-06-02 19:27:52 +0900

    Update bitpartite_matching.h

--- headers/bitpartite_matching.h
+++ headers/bitpartite_matching.h
@@ -1,40 +1,82 @@
 //
 //  bitpartite_matching.h
-//  0903
 //
-//  Created by IRIS on 9/3/15.
+//  Updated by Prev on 16.6.2
 //
+
+#include <stdio.h>
+#include <string.h>
+#include <vector>
 
 #pragma once
 
-#ifndef _903_bitpartite_matching_h
-#define _903_bitpartite_matching_h
+#ifndef _bitpartite_matching_h
+#define _bitpartite_matching_h
 #endif
 
-vector<vector<int> > v;
-int backMatch[max_N*max_N];
-int column_cnt[max_N*max_N];
-bool visited[max_N*max_N];
- 
-bool dfs(int now) {
-    if (visited[now]) return false;
-    visited[now] = true;
-        
-    for (auto next: v[now]) {
-        if (backMatch[next] == -1 || dfs(backMatch[next])) {
-            backMatch[next] = now;
-            return true;
-        }
-    }
-    return false;
+using namespace std;
+
+vector<vector<int> > adj;
+vector<int> a_match, b_match;
+vector<bool> visited;
+
+bool dfs(int a){
+	if (visited[a]) return false;
+	visited[a] = true;
+	
+	for (auto b: adj[a]) {
+		if (b_match[b] == -1 || dfs(b_match[b])) {
+			a_match[a] = b;
+			b_match[b] = a;
+			return true;
+		}
+	}
+	
+	return false;
 }
-int BitpartiteMatching() {
-    memset(backMatch, -1, sizeof(backMatch));
-         
-    int matched = 0;
-    for (size_t i=0; i<v.size(); i++) {
-        memset(visited, false, sizeof(visited));
-        if (dfs(i)) matched++;
-    }
-    return matched;
+
+int bipartiteMatch(int n, int m){
+	a_match.assign(n, -1);
+	b_match.assign(m, -1);
+	
+	int ret = 0;
+	
+	for (int s = 0; s < n; ++s){
+		visited.assign(n, false);
+		if (dfs(s)) ++ret;
+	}
+	
+	return ret;
 }
+
+/*
+boj 11375
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	
+	adj.resize(n);
+	
+	for (int i=0; i<n; i++) {
+		int k;
+		scanf("%d", &k);
+		
+		for (int j=0; j<k; j++) {
+			int x;
+			scanf("%d", &x);
+			
+			adj[i].push_back( x-1 );
+		}
+	}
+	
+	printf("%d", bipartiteMatch(n, m));
+	
+	return 0;
+}
+*/
+
+
+
+
+
